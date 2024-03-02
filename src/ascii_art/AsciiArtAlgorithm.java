d2e137c00a957b8772af28f4adc88bc29a2f674a
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2024-03-02 19:25:46 +0200

    changed matcher to be static

--- src/ascii_art/AsciiArtAlgorithm.java
+++ src/ascii_art/AsciiArtAlgorithm.java
@@ -8,11 +8,9 @@ import image_char_matching.SubImgCharMatcher;
  *  This class executes the ascii art algorithm.
  */
 public class AsciiArtAlgorithm {
-
-//    private SubImgCharMatcher matcher;
-    private final char[] asciiChars;
-    private Image img;
-    private int resolution;
+    static private SubImgCharMatcher matcher;
+    private final Image img;
+    private final int resolution;
 
     /**
      * Constructor for the class used by the user.
@@ -21,7 +19,13 @@ public class AsciiArtAlgorithm {
      * @param asciiChars the charset to use for thr art
      */
     public AsciiArtAlgorithm(Image img, int resolution, char[] asciiChars) {
-        this.asciiChars = asciiChars;
+        if (matcher == null) {
+            matcher = new SubImgCharMatcher(asciiChars);
+        } else {
+            for (char c : asciiChars) {
+                matcher.addChar(c);
+            }
+        }
         this.img = img;
         this.resolution = resolution;
     }
@@ -31,17 +35,8 @@ public class AsciiArtAlgorithm {
      * @return the array of characters that represents the ascii art image.
      */
     public char [][] run() {
-        SubImgCharMatcher matcher = new SubImgCharMatcher(asciiChars);
         ImageToAsciiConverter converter = new ImageToAsciiConverter(this.img,this.resolution,matcher);
         return converter.convertImageToAsciiArt();
-    }
-
-    public void setImage(Image img) {
-        this.img = img;
-    }
-
-    public void setResolution(int resolution) {
-        this.resolution = resolution;
     }
 }
 
